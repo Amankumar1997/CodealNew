40e7f01a891e7afc20732f3eeebafdc0d123b5d9
Amankumar1997 <38433641+Amankumar1997@users.noreply.github.com>
2022-02-21 10:58:13 +0530

    restricting the form to be visible to the part only whenn the user signed in

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+const passport=require('passport');
 // access home controller 
 const postController=require('../controllers/post_controller');
 
 
 // map  a route to the post controller
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication,postController.create);
 
 
 
